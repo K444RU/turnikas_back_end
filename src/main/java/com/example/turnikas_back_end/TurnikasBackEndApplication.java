9a83570588fdd6016c80edeaea1a74c5b07461bb
olegtrofimov <olegtrofimov96@gmail.com>
2023-03-16 15:09:53 +0200

    Initial commit

---
+++ src/main/java/com/example/turnikas_back_end/TurnikasBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.turnikas_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnikasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnikasBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/turnikas_back_end/TurnikasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turnikas_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnikasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
