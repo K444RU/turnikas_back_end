6248f41defed839d8e41b6ae833f840dd79b317a
olegtrofimov <olegtrofimov96@gmail.com>
2024-07-05 17:40:08 +0300

    TURN | Create tournament registration for teams via participation
    -New classes created:
    -ParticipationController, ParticipationDTO, ParticipationRepository, ParticipationService

---
+++ src/main/java/com/example/turnikas_back_end/business/team/controller/ParticipationController.java
@@ -0,0 +1,28 @@
+package com.example.turnikas_back_end.business.team.controller;
+
+import com.example.turnikas_back_end.business.team.dto.ParticipationDTO;
+import com.example.turnikas_back_end.business.team.service.ParticipationService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/participation")
+public class ParticipationController {
+
+    private final ParticipationService participationService;
+
+    @Autowired
+    public ParticipationController(ParticipationService participationService) {
+        this.participationService = participationService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Register team for a tournament")
+    public void registerTeamForTournament(@RequestBody ParticipationDTO participationDTO) {
+        participationService.registerTeamForTournament(participationDTO);
+    }
+}
---
+++ src/main/java/com/example/turnikas_back_end/business/team/service/ParticipationService.java
@@ -0,0 +1,27 @@
+package com.example.turnikas_back_end.business.team.service;
+
+import com.example.turnikas_back_end.business.team.dto.ParticipationDTO;
+import com.example.turnikas_back_end.business.team.repository.ParticipationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ParticipationService {
+
+    private final ParticipationRepository participationRepository;
+
+    @Autowired
+    public ParticipationService(ParticipationRepository participationRepository) {
+        this.participationRepository = participationRepository;
+    }
+
+    @Transactional
+    public void registerTeamForTournament(ParticipationDTO participationDTO) {
+        if (!participationRepository.existsByTeamIdAndTournamentId(participationDTO.getTeamId(), participationDTO.getTournamentId())) {
+            participationRepository.save(participationDTO.getTeamId(), participationDTO.getTournamentId());
+        } else {
+            throw new IllegalArgumentException("Team is already registered for this tournament.");
+        }
+    }
+}
